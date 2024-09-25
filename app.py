10c8aa42a92a19525fcc6decec311ffc033174a8
fsohrabi <fs.sohrabi70@gmail.com>
2024-09-25 22:08:34 +0200

    add page add html and logic

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import render_template, Flask
+from flask import render_template, Flask, request, redirect, url_for
 from storage.storage_json import StorageJson
 
 app = Flask(__name__)
@@ -9,6 +9,17 @@ storage = StorageJson()
 def index():
     blogs = storage.list_blogs()
     return render_template('index.html', blogs=blogs)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        author = request.form['author']
+        storage.add_blog(title, content, author)
+        return redirect(url_for('index'))
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
